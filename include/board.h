f1850f75ae7652f4d70c4dabb6a10d6ec6865cb6
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-05-20 16:02:10 -0600

    update: board class method printBoard

--- include/board.h
+++ include/board.h
@@ -1,16 +1,16 @@
 #include <vector>
+#include <iostream>
 
 class board
 {
 private:
   int rows;
   int columns;
-  int total_positions;
+  int pos;
 
 public:
   board();
   board(int rows, int columns);
   ~board();
-  void startBoard();
-  void updateBoard(std::vector<int> players_pos);
+  void printBoard(std::vector<int> players_pos);
 };
--- src/board.cpp
+++ src/board.cpp
@@ -4,13 +4,13 @@ board::board()
 {
   rows = 10;
   columns = 10;
-  total_positions = rows * columns;
+  pos = 1;
 }
 
 board::board(int rows, int columns)
     : rows(rows), columns(columns)
 {
-  total_positions = rows * columns;
+  pos = 1;
 }
 
 board::~board()
@@ -18,4 +18,16 @@ board::~board()
 }
 void board::printBoard(std::vector<int> player_pos)
 {
+  int spacing = 0;
+  int start_x;
+  int start_y;
+
+  for (int y = 0; y < rows; y++)
+  {
+    for (int x = 0; x < columns; x++)
+    {
+      std::cout << pos;
+      pos++;
+    }
+  }
 }
